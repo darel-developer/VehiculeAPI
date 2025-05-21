4b16a7bcbac59be6370ad90f90b0f0ee4b30abf4
Divine-NOUDOU <146577345+Divine-NOUDOU@users.noreply.github.com>
2025-05-21 10:50:21 +0100

    Implementation de la classe UserService

--- src/main/java/com/propelize/vehicleapi/service/UserService.java
+++ src/main/java/com/propelize/vehicleapi/service/UserService.java
@@ -1,5 +1,40 @@
 package com.propelize.vehicleapi.service;
 
-public abstract class UserService {
-    
+import com.propelize.vehicleapi.model.User;
+import com.propelize.vehicleapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Créer ou mettre à jour un utilisateur
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void setUserRepository(UserRepository repo) {
+        this.userRepository = repo;
+    }
+
+    // Récupérer tous les utilisateurs
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Récupérer un utilisateur par ID
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    // Supprimer un utilisateur
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
