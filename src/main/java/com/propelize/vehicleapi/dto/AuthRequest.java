ca7c3495b4cba879884ca84cd1a75607d3f6a9f8
darel-developer <sanangdarel17@gmail.com>
2025-05-23 17:10:32 +0100

    mise à jour AuthRequest correction

--- src/main/java/com/propelize/vehicleapi/dto/AuthRequest.java
+++ src/main/java/com/propelize/vehicleapi/dto/AuthRequest.java
@@ -1,32 +1,29 @@
 package com.propelize.vehicleapi.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public class AuthRequest {
 
-    @NotBlank(message = "Le nom d'utilisateur est obligatoire")
-    private String username;
+    @NotBlank
+    private String name;
 
-    @NotBlank(message = "Le mot de passe est obligatoire")
-    @Size(min = 4, message = "Le mot de passe doit contenir au moins 4 caractères")
+    @NotBlank
     private String password;
 
     public AuthRequest() {
     }
 
-    public AuthRequest(String username, String password) {
-        this.username = username;
+    public AuthRequest(String name, String password) {
+        this.name = name;
         this.password = password;
     }
 
-    // Getters et Setters
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPassword() {
