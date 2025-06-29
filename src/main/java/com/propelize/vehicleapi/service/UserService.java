895ed6e91f71c3b3a6b39979a15d591d6cee3b16
Wally🤖 <williams25prince@gmail.com>
2025-06-29 10:43:24 +0100

    Remove password encoding from UserService
    Eliminated usage of PasswordEncoder in UserService. Passwords are now stored and compared in plain text instead of being hashed. This affects user registration and authentication logic.

--- src/main/java/com/propelize/vehicleapi/service/UserService.java
+++ src/main/java/com/propelize/vehicleapi/service/UserService.java
@@ -4,7 +4,6 @@ import com.propelize.vehicleapi.model.User;
 import com.propelize.vehicleapi.repository.UserRepository;
 import com.propelize.vehicleapi.dto.AuthRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,26 +18,11 @@ public class UserService {
     @Autowired
     private JwtUtil jwtUtil;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     public User saveUser(User user) {
         if (userRepository.findByNameIgnoreCase(user.getName()).isPresent()) {
             throw new RuntimeException("Ce nom d'utilisateur existe déjà.");
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
-    }
-
-    public String authenticate(AuthRequest authRequest) {
-        Optional<User> userOpt = userRepository.findByName(authRequest.getName());
-        if (userOpt.isPresent()) {
-            User user = userOpt.get();
-            if (passwordEncoder.matches(authRequest.getPassword(), user.getPassword())) {
-                return jwtUtil.generateToken(user.getName());
-            }
-        }
-        throw new RuntimeException("Identifiants invalides");
     }
 
     public void setUserRepository(UserRepository repo) {
@@ -59,5 +43,16 @@ public class UserService {
 
     public Optional<User> findByName(String name) {
         return userRepository.findByName(name);
+    }
+
+    public String authenticate(AuthRequest authRequest) {
+        Optional<User> userOpt = userRepository.findByName(authRequest.getName());
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            if (user.getPassword().equals(authRequest.getPassword())) {
+                return jwtUtil.generateToken(user.getName());
+            }
+        }
+        throw new RuntimeException("Identifiants invalides");
     }
 }
